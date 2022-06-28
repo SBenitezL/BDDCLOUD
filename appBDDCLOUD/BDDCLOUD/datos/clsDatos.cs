06d2d3e659bacb4bf4563f043723d4ddedf8058e
Santiago Benitez <sbenitez1607@gmail.com>
2022-06-28 17:55:12 -0500

    Se agrega las clases de logica y datos, ademas de la cadena de conexion funciuonal para la clase datos

---
+++ appBDDCLOUD/BDDCLOUD/datos/clsDatos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BDDCLOUD.datos
+{
+    class clsDatos
+    {
+        #region Atributos
+        SqlConnection atrConection;
+        #endregion
+        #region Metodos
+        #region constructor
+        public clsDatos()
+        {
+            SqlConnectionStringBuilder varCadena = new SqlConnectionStringBuilder
+            {
+                DataSource = "bddcloudserver.database.windows.net",
+                UserID = "BDDCLOUDUSER",
+                Password = "DUOLCddb1",
+                InitialCatalog = "BDDVIDEOJUEGOS"
+            };
+            atrConection = new SqlConnection(varCadena.ConnectionString);
+        }
+        #endregion
+        
+       
+        #endregion
+    }
+}
